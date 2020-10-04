dc3117dfb0a32c77bebfa6f1467b70674f31dd54
Vasek <vasek.gm@gmail.com>
2020-10-04 22:10:18 +0300

    init

---
+++ MySuperService/Controllers/SampleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MySuperService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SampleController : ControllerBase
+    {
+        ILogger _logger;
+
+        public SampleController(ILogger<SampleController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("Add")]
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ MySuperService/Startup.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MySuperService
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
